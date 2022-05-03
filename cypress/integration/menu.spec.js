cdfa94f9f0b8bd2c66047e3f75aa010c8276fa19
RODRIGO REGINALDO DOS SANTOS COELHO <rodrigo.coelho@portoseguro.com.br>
2022-05-03 03:47:24 -0300

    Some maintenance in the executions of the tests, and upload of the success reports.

--- cypress/integration/menu.spec.js
+++ cypress/integration/menu.spec.js
@@ -6,11 +6,11 @@ describe('Menu', () => {
 
     beforeEach(() => {
 
-        cy.visit('https://rain.us/') 
+        cy.visit('https://rain.us/')
     })
 
     it('Validate "About us" page...', () => {
-        
+
         cy.get(loc.MENU.ABOUT_US).click()
         cy.get(loc.ABOUT_US.TITLE).should('have.text', 'Our mission')
         cy.get(loc.ABOUT_US.TEXT).should('not.be.empty')
@@ -19,7 +19,7 @@ describe('Menu', () => {
     })
 
     it('Validate "How it works" page...', () => {
-        
+
         cy.get(loc.MENU.HOW_IT_WORKS).click()
         cy.get(loc.HOW_IT_WORKS.TITLE).should('have.text', 'How It Works')
         cy.get(loc.HOW_IT_WORKS.TEXT).should('not.be.empty')
@@ -35,32 +35,36 @@ describe('Menu', () => {
     })
 
     it('Validate "Blog" page...', () => {
-        
+
         cy.get(loc.MENU.BLOG).click()
         cy.title().should('be.equal', 'Blog')
 
         cy.get(loc.BLOG.TAB_FINANCIAL_WELLNESS).click()
         cy.get(loc.BLOG.TITLE_FINANCIAL_WELLNESS).should('have.text', "Financial Wellness")
-        cy.wait(1000)
-        
+        cy.wait(500)
+
         cy.get(loc.BLOG.TAB_BUSINESS).click()
         cy.get(loc.BLOG.TITLE_BUSINESS).should('have.text', "Business")
+        cy.wait(500)
 
         cy.get(loc.BLOG.TAB_HUMAN_RESOURCES).click()
         cy.get(loc.BLOG.TITLE_HUMAN_RESOURCES).should('have.text', "Human Resources")
+        cy.wait(500)
 
         cy.get(loc.BLOG.TAB_ANNOUNCEMENTS).click()
         cy.get(loc.BLOG.TITLE_ANNOUNCEMENTS).should('have.text', "Announcements")
     })
 
     it('Validate "Contact" page...', () => {
-        
+
         cy.get(loc.MENU.CONTACT).click()
         cy.title().should('be.equal', 'Contact')
+        cy.wait(500)
 
         cy.get(loc.CONTACT.TITLE).should('have.text', 'Let’s chat!')
         cy.get(loc.CONTACT.TEXT).should('have.text', 'Have a question? You can ask us anything about Rain!')
         cy.get(loc.CONTACT.TEXT2).should('have.text', 'For customer service questions, please reach out at care@rain.usand 424-369-RAIN')
+        cy.wait(500)
 
         cy.get(loc.CONTACT.IPT_FIRT_NAME).type('Rodrigo')
         cy.get(loc.CONTACT.IPT_LAST_NAME).type('Santos')
@@ -70,17 +74,21 @@ describe('Menu', () => {
         cy.get(loc.CONTACT.IPT_EMPLOYEE_COUNT).select('100-500')
         cy.get(loc.CONTACT.IPT_JOB_TITLE).type('QA Engineer')
         cy.get(loc.CONTACT.IPT_MESSAGE).type('Message to validate Rain website contact page for QA Engineer challenge.')
-        //cy.get(loc.CONTACT.BTN_SUBMIT).click()
+        //cy.get(loc.CONTACT.BTN_SUBMIT).click() ## Commented to don't send real message for the company.
+        cy.wait(500)
 
         cy.get(loc.CONTACT.TEXT3).should('have.text', 'This site is protected by reCAPTCHA and the GooglePrivacy Policy andTerms of Service apply.')
     })
 
     it('Validate mandatory fiels...', () => {
-        
+
         cy.get(loc.MENU.CONTACT).click()
         cy.title().should('be.equal', 'Contact')
+        cy.wait(500)
 
         cy.get(loc.CONTACT.BTN_SUBMIT).click()
+        cy.wait(500)
+
         cy.get(loc.CONTACT.MSG_REQUIRED_FIELD_NAME).should('have.text', 'Please complete this required field.')
         cy.get(loc.CONTACT.MSG_REQUIRED_FIELD_LAST).should('have.text', 'Please complete this required field.')
         cy.get(loc.CONTACT.MSG_REQUIRED_FIELD_EMAIL).should('have.text', 'Please complete this required field.')
@@ -90,17 +98,19 @@ describe('Menu', () => {
     })
 
     it('Validate message to invalid login...', () => {
-        
+
         cy.get(loc.MENU.EMPLOYERS_ACCESS).click()
+        cy.wait(500)
         cy.get(loc.EMPLOYERS_ACCESS.TITLE).should('have.text', 'Give your people control over their income and finances')
         cy.get(loc.EMPLOYERS_ACCESS.EMAIL).type('rodrigo.santosrr@gmail.com')
         cy.get(loc.EMPLOYERS_ACCESS.PASSWORD).type('rain123')
         cy.get(loc.EMPLOYERS_ACCESS.SIGN_IN).click()
+        cy.wait(500)
         cy.get(loc.EMPLOYERS_ACCESS.MSG_ERROR).should('have.text', 'Could not sign in, invalid email or password')
     })
 
     it('Validate Forgot Password...', () => {
-        
+
         cy.get(loc.MENU.EMPLOYERS_ACCESS).click()
         cy.get(loc.EMPLOYERS_ACCESS.TITLE).should('have.text', 'Give your people control over their income and finances')
 
